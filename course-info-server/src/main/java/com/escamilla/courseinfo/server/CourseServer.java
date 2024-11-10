e5b8daa00b45fba8179995a34551723b905a121f
EscamillaJuan <juanpedroescamilla@outlook.com>
2024-11-10 15:15:33 -0600

    refactor: properties & dependencies

--- course-info-server/src/main/java/com/escamilla/courseinfo/server/CourseServer.java
+++ course-info-server/src/main/java/com/escamilla/courseinfo/server/CourseServer.java
@@ -7,7 +7,10 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.util.Properties;
 import java.util.logging.LogManager;
 
 public class CourseServer {
@@ -18,12 +21,24 @@ public class CourseServer {
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
-    private static final String BASE_URI = "http://localhost:8080/";
+    private static final String BASE_URI = loadProperties().getProperty("course-info.uri");
 
     public static void main(String... args) {
-        LOG.info("Starting HTTP server");
-        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        String databaseFile = loadProperties().getProperty("course-info.database");;
+        LOG.info("Starting HTTP server {} with database {}", BASE_URI, databaseFile);
+        CourseRepository courseRepository = CourseRepository.openCourseRepository(databaseFile);
         ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
         GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
+
+    private static Properties loadProperties() {
+        try (InputStream propertiesStream =
+                CourseServer.class.getResourceAsStream("/server.properties")) {
+            Properties properties = new Properties();
+            properties.load(propertiesStream);
+            return properties;
+        } catch (IOException e) {
+            throw new IllegalStateException("Could not load properties");
+        }
     }
 }
